9335c17077be63688f28609e115312e94a61dd5d
tusharpasricha <tusharpasricha21@gmail.com>
2023-07-28 14:45:28 +0530

    single room to single person

--- src/components/Room/AllotRoom.js
+++ src/components/Room/AllotRoom.js
@@ -1,94 +1,80 @@
 import React, { useContext, useRef, useState } from "react";
-import {useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { collection, addDoc, query, where, getDocs } from "firebase/firestore";
 
 import Sidebar from "../../auth/sidebar";
 import Third from "../../design/third";
 
 import AuthContext from "../../stores/AuthContext";
-
 import db from "../../data/firebase";
-import { collection, deleteDoc, doc, getDocs, orderBy, query, where } from "firebase/firestore";
-import {  addDoc } from "firebase/firestore";
 
 const AllotRoom = () => {
   const navigation = useNavigate();
-
   const roomRef = useRef();
   const personRef = useRef();
   const dateRef = useRef();
   const rentRef = useRef();
 
-  const {user} = useContext(AuthContext);
-
+  const { user } = useContext(AuthContext);
   const [loading, setLoading] = useState(false);
 
-  function daysInMonth(month, year) {
-    return new Date(year, month, 0).getDate();
-  }
-
-  const submitHandler = (e) => {
+  const submitHandler = async (e) => {
     e.preventDefault();
     setLoading(true);
-    console.log("Alloting the room..")
+    console.log("Alloting the room..");
+
     const userId = user.uid;
-    const roomNo = roomRef.current.value;
-    const personName = personRef.current.value;
-    const rent = parseFloat(rentRef.current.value);
-    const year = parseInt(dateRef.current.value.split("-")[0]);
-    const month = parseInt(dateRef.current.value.split("-")[1]);
-    const day = parseInt(dateRef.current.value.split("-")[2]);
+    const roomNo = parseInt(roomRef.current.value);
 
-    console.log("date:" + dateRef.current.value);
-    const selectedDate = new Date(dateRef.current.value);
-    const nextDate = new Date(selectedDate);
-    nextDate.setMonth(nextDate.getMonth() + 1);
-    const next = `${nextDate.getFullYear()}-${(nextDate.getMonth() + 1).toString().padStart(2, '0')}-${nextDate.getDate().toString().padStart(2, '0')}`;
-    console.log("next date:" + next);
+    // Query to find all rooms belonging to the user
+    const userRoomsQuery = query(collection(db, "rooms"), where("userId", "==", userId));
+    const userRoomsSnapshot = await getDocs(userRoomsQuery);
 
-    //checking if already alloted 
-    if (user) {
-      console.log("checking if already alloted ");
-      const q = query(collection(db, "rooms"), where("userId", "==", userId), where("roomNo", "==", roomNo));
+    // Check if there is already a room with the specified roomNo
+    const alreadyAlloted = userRoomsSnapshot.docs.some((doc) => doc.data().roomNo === roomNo);
 
-      getDocs(q)
-      .then((querySnapshot) => {
-        const docs = querySnapshot.docs;
-        console.log("doc lenght" +docs.length)
-        if(docs.length > 0){
-          console.log("Room already alloted");
-          alert("Room already allotted.");
-        }
-        else{
-          addDoc(collection(db, "rooms"), {
-            roomNo: parseInt(roomNo),
-            personName: personName,
-            month: month,
-            day: day,
-            year: year,
-            next: next,
-            rent: rent,
-            amount: rent,
-            paid: 0,
-            userId: userId,
-          })
-            .then(() => {
-              console.log("New Room Added Successfully");
-              alert("New Room Added Successfully");
-              e.target.reset();
-              setLoading(false);
-            })
-            .catch((err) => {
-              console.log(err);
-            });
+    if (alreadyAlloted) {
+      console.log("Room already allotted");
+      alert("Room already allotted to you.");
+      setLoading(false);
+    } else {
+      const personName = personRef.current.value;
+      const rent = parseFloat(rentRef.current.value);
+      const year = parseInt(dateRef.current.value.split("-")[0]);
+      const month = parseInt(dateRef.current.value.split("-")[1]);
+      const day = parseInt(dateRef.current.value.split("-")[2]);
 
-        }
+      console.log("date:" + dateRef.current.value);
+      const selectedDate = new Date(dateRef.current.value);
+      const nextDate = new Date(selectedDate);
+      nextDate.setMonth(nextDate.getMonth() + 1);
+      const next = `${nextDate.getFullYear()}-${(nextDate.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}-${nextDate.getDate().toString().padStart(2, "0")}`;
+      console.log("next date:" + next);
 
-        
-      }).catch(err => {
-          console.log(err);
-      })
-  }
-
+      try {
+        await addDoc(collection(db, "rooms"), {
+          roomNo: parseInt(roomNo),
+          personName: personName,
+          month: month,
+          day: day,
+          year: year,
+          next: next,
+          rent: rent,
+          amount: rent,
+          paid: 0,
+          userId: userId,
+        });
+        console.log("New Room Added Successfully");
+        alert("New Room Added Successfully");
+        e.target.reset();
+        setLoading(false);
+      } catch (err) {
+        console.log("Error adding the new room:", err);
+        setLoading(false);
+      }
+    }
   };
 
   return (
